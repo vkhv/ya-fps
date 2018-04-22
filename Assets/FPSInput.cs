779b74bfaaf30ca30829e9c61b4a032b5f4515c1
vkhv <vladkhvo@gmail.com>
2018-04-22 16:29:34 +0300

    add gravity and wall real

--- Assets/FPSInput.cs
+++ Assets/FPSInput.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class FPSInput : MonoBehaviour {
     public float speed = 15.0f;
+    public float gravity = -9.8f;
+    private CharacterController _charController;
+
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        _charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update () {
         float deltaX = Input.GetAxis("Horizontal") * speed;
         float deltaZ = Input.GetAxis("Vertical") * speed;
 
-        transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
-	}
+        // transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+        movement.y = gravity;
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _charController.Move(movement);
+    }
 }
